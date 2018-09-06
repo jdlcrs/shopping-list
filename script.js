d07f5676257a18154429ac14bfca25d9325829b7
jdlcrs <jdlcrs@users.noreply.github.com>
2018-09-06 11:49:00 +0200

    Sending my files to the nuages :)

---
+++ script.js
@@ -0,0 +1,34 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("user-input");
+var ul = document.querySelector("ul");
+var introMessage = document.getElementById("1li");
+
+function inputLength() {
+   return input.value.length;
+}
+
+function removeIntroMessage() {
+    var removeLi = document.removeChild(introMessage);
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+        if (inputLength() > 0) {
+            createListElement();
+    }
+}
+
+function addListAfterKeypress (event) {
+        if (inputLength() > 0 && event.keyCode === 13) {
+            createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
